328dfbabf73423a79662e6a6465fe4312304cbc7
Felix Kranich <felix.kranich@me.com>
2014-11-17 19:51:48 +0100

    First commit(since fork)

--- main.rs
+++ main.rs
@@ -52,9 +52,41 @@ fn clear_screen(background: Color) {
         }
     }
 }
-
+fn make_vgaentry(c: u8, fg: Color, bg: Color) -> u16 {
+    let color = fg as u16 | (bg as u16 << 4);
+    return c as u16 | (color << 8);
+}
+fn put_char(x: u16, y: u16, c: u8) {
+    let idx : uint =  (y * 30 * 2 + x * 2) as uint;
+    unsafe {
+        *((0xb8000 + idx) as *mut u16) = make_vgaentry(c, Black, White);
+    }
+}
+// doesnt work
+//
+// fn sleep(ticks : int) // maybe some other data type like long?
+// {
+//     let mut eticks : int = 0;
+//     while eticks < ticks {
+//       eticks = eticks+1;
+//     }
+//
+// }
 #[no_mangle]
 #[no_split_stack]
 pub fn main() {
-    clear_screen(LightRed);
+    clear_screen(White);
+    put_char(1,0,'H' as u8);
+    put_char(2,0,'e' as u8);
+    put_char(3,0,'l' as u8);
+    put_char(4,0,'l' as u8);
+    put_char(5,0,'o' as u8);
+    put_char(6,0,',' as u8);
+    put_char(7,0,' ' as u8);
+    put_char(8,0,'W' as u8);
+    put_char(9,0,'o' as u8);
+    put_char(10,0,'r' as u8);
+    put_char(11,0,'l' as u8);
+    put_char(12,0,'d' as u8);
+    put_char(13,0,'!' as u8);
 }
